0385ecd36b19fb33b44f52be2858bcae922d17d2
Fernando Bahamondes <ferbaco@gmail.com>
2020-04-24 18:21:56 -0400

    Add my_each_with_index method and test case

--- enumerables.rb
+++ enumerables.rb
@@ -4,9 +4,13 @@ module Enumerable
       yield(value)
     end
   end
+
+  def my_each_with_index
+    my_each do |item|
+      yield(item, index(item))
+    end
+  end
 end
-
-my_array = [1,2,3,4,5,6]
-
-my_array.my_each {|num| puts num * 2}
-
+# my_array = [1, 2, 3, 4, 5, 6]
+# my_array.my_each { |num| puts num * 2 }
+%w[cat dog mouse].my_each_with_index { |item, index| puts "#{item} #{index}" }
