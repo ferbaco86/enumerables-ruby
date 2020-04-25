db738896da0d0f2f5f55cb8ca85cb905ed3136af
Fernando Bahamondes <ferbaco@gmail.com>
2020-04-24 23:12:05 -0400

    Add return enumerator if no block is given to methods

--- enumerables.rb
+++ enumerables.rb
@@ -1,11 +1,13 @@
 module Enumerable
   def my_each
+    return enum_for(:my_each) unless block_given?
     for value in self
       yield(value)
     end
   end
 
   def my_each_with_index
+    return enum_for(:my_each_with_index) unless block_given?
     my_each do |item|
       yield(item, index(item))
     end
@@ -13,4 +15,4 @@ module Enumerable
 end
 # my_array = [1, 2, 3, 4, 5, 6]
 # my_array.my_each { |num| puts num * 2 }
-%w[cat dog mouse].my_each_with_index { |item, index| puts "#{item} #{index}" }
+puts %w[cat dog mouse].my_each_with_index
