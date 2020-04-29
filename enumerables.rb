8cfd7fa3928278d6571c55afeb6334b514f3ea5f
Fernando Bahamondes <ferbaco@gmail.com>
2020-04-29 01:11:57 -0400

    Add description for each method. Fix rubocop offenses

--- enumerables.rb
+++ enumerables.rb
@@ -1,4 +1,6 @@
 module Enumerable
+  # This method will execute the block for each of the array's elements.
+
   def my_each
     return enum_for(:my_each) unless block_given?
 
@@ -16,6 +18,10 @@ module Enumerable
     # end
   end
 
+  # Calls block with two arguments, the item and its index, for each item in enum.
+  # Given arguments are passed through to each().
+  # If no block is given, an enumerator is returned instead.
+
   def my_each_with_index
     return enum_for(:my_each_with_index) unless block_given?
 
@@ -28,99 +34,30 @@ module Enumerable
     end
   end
 
+  # Returns an array containing all elements of enum for which the given block returns a true value.
+
   def my_select
     return enum_for(:my_select) unless block_given?
-    arr = []
-    cont = 0
 
+    arr = []
     my_each do |item|
-        if yield(item)
-          arr.push(item)
-        end                          
-      end
-      arr
+      arr.push(item) if yield(item)
+    end
+    arr
   end
 
+  # Passes each element of the collection to the given block.
+  # The method returns true if the block never returns false or nil.
+  # If the block is not given, Ruby adds an implicit block of { |obj| obj }
+  # which will cause all? to return true when none of the collection members are false or nil.
+  # If instead a pattern is supplied, the method returns whether pattern === element for every collection member.
+
   def my_all?
-    true_elements = []
+    false_elements = []
     my_each do |item|
-      unless item == false || item == nil
-        true_elements.push(item)
-      elsif
-        true_elements.my_each do |element|
-        end 
-        return false
-      end
+      p item
+      false_elements.push(item) if !item || item.nil?
     end
-    end
-
-  
+    false_elements.empty? ? true : false
+  end
 end
-
- #p [false, false, 2].all?(false) 
- #p [false, false, 2].all?{|num| num > 1} (false)
- 
- p [4, nil, 3].my_all?
- # p [nil, nil, nil].all?
-
-#p [3, 3, false].my_all? 
-#p [3, 3, false].all? 
-
-#p [false, false, false].my_all? 
-#p [false, false, false].all? 
-
-
-# my_array = [1, 2, 3, 4, 5, 6]
-# my_string = 'Fernando'
-# my_hash = { 'Fernando' => 1986, 'Rocio' => 1995, 'Joaquin' => 1988 }
-
-# p my_array.all? { |num| num < 7 }
-# p my_array.my_all? { |num| num < 7 }
-# p %w[ant bear cat].all? { |word| word.length >= 4 }
-# p %w[ant bear cat].my_all? { |word| word.length >= 4 }
-# p [1, 2i, 3.14].all?(String)
-# p [1, 2i, 3.14].my_all?(String)
-
-# p [true, true, true].all?
-# p [true, true, true].my_all?
-
-# p [].all?
-# p [].my_all? 
-
-# my_array.each { |num| puts "Each original method: #{num}" }
-# puts "Array each original method: #{ my_array.each {|num|} }"
-# puts "Array my_each method: #{ my_array.my_each {|num|} }"
-# my_array.my_each { |num| puts "My Each method: #{num}" }
-
-# my_array.each_with_index { |item, index| puts "each_with_index original method:#{item} #{index}" }
-# puts "Array each_with_index original method: #{ my_array.each_with_index {|item, index|} }"
-# puts "Array my_each_with_index method: #{ my_array.my_each_with_index {|item, index|} }"
-# my_array.my_each_with_index { |item, index| puts "my_each_with_index method: #{item} #{index}" }
-
-# my_array.select { |num| puts "select original method:#{num} #{num.even?}" }
-# my_array.my_select { |num| puts "my_select  method:#{num} #{num.even?}" }
-
-# puts "Array select original method : #{ my_array.select {|num| num.even? }}"
-# puts "Array my_select method : #{ my_array.my_select {|num| num.even? }}"
-
-# puts "select original method : #{ my_array.select {|num| num > 2 }}"
-# my_array.select { |num| puts " #{num} is greater than 2?: #{num > 2 }"}
-# my_array.my_select { |num| puts " #{num} is greater than 2?: #{num > 2 }"}
-# puts "my_select method : #{ my_array.my_select {|num| num > 2 }}"
-
-# my_hash.each { |item, index| puts "each original method: #{item} #{index}" }
-# puts "Hash each original method: #{ my_hash.each_with_index {|item, index|} }"
-# puts "Hash my_each method: #{ my_hash.my_each_with_index {|item, index|} }"
-# my_hash.my_each { |item, index| puts "my_each method: #{item} #{index}" }
-
-# my_hash.each_with_index { |item, index| puts "each_with_index original method: #{item} #{index}" }
-# puts "Hash each_with_index original method: #{ my_hash.each_with_index {|item, index|}}"
-# puts "Hash my_each_with_index method: #{ my_hash.my_each_with_index {|item, index|}}"
-# my_hash.my_each_with_index { |item, index| puts "my_each_with_index method: #{item} #{index}" }
-
-# puts "Hash select original method : #{ my_hash.select {|key, value| value > 1990 }}"
-# puts "Hash my_select original method : #{ my_hash.my_select {|key, value| value > 1990 }}"
-
-
-
-# %w[cat dog mouse].my_each_with_index {|pet, index| puts "#{pet} #{index}"}
