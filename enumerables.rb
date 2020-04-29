d010bbc204fd3d6f69c37cc9a1ba692d13cb50bf
Fernando Bahamondes <ferbaco@gmail.com>
2020-04-29 18:56:49 -0400

    First draft of my_inject method  working when symbol is passed as a parameter

--- enumerables.rb
+++ enumerables.rb
@@ -145,4 +145,21 @@ module Enumerable
     end
     new_arr
   end
+
+  # Combines all elements of enum by applying a binary operation, specified by a block or a symbol that names a method or operator.
+  # If you specify a block, then for each element in enum the block is passed an accumulator value (memo) and the element. 
+  # If you specify a symbol instead, then each element in the collection will be passed to the named method of memo. 
+  # In either case, the result becomes the new value for memo. At the end of the iteration, the final value of memo is the return value for the method.
+  # If you do not explicitly specify an initial value for memo, then the first element of collection is used as the initial value of memo.
+
+  def my_inject(*args)
+    arr = *self
+    temp = arr.shift unless args.nil? || args.length == 2
+    p temp
+    arr.my_each do |item|
+      temp = temp.send(args[0].to_s, item)
+    end
+    temp
+  end
 end
+
